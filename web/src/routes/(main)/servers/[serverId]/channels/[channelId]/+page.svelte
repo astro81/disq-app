fdc6b8a7dae8a7c6108f9e4216a8293ff18eceaa
astro81 <caien720@protonmail.com>
2026-03-29 08:57:13 +0545

    voice merge

--- web/src/routes/(main)/servers/[serverId]/channels/[channelId]/+page.svelte
+++ web/src/routes/(main)/servers/[serverId]/channels/[channelId]/+page.svelte
@@ -9,6 +9,9 @@
   import VideoControls from '$lib/components/video/VideoControls.svelte'
 
   import type { PageProps } from './$types'
+	import { joinVoiceChannel, leaveVoiceChannel } from '$lib/stores/voice.svelte';
+	import VoiceGrid from '$lib/components/voice/VoiceGrid.svelte';
+	import VoiceControls from '$lib/components/voice/VoiceControls.svelte';
 
   let { data }: PageProps = $props()
 
@@ -27,6 +30,13 @@
     if (channelType === 'video') {
       joinVideoChannel(currentChannel.channelId, currentMember)
       return () => leaveVideoChannel()
+    }
+  })
+
+    $effect(() => {
+    if (channelType === 'voice') {
+      joinVoiceChannel(currentChannel.channelId, currentMember)
+      return () => leaveVoiceChannel()
     }
   })
 
@@ -70,8 +80,29 @@
   </div>
 
 {:else if channelType === 'voice'}
-  <div class="flex h-screen items-center justify-center">
-    <p class="text-zinc-400">Voice channels coming soon</p>
+  <div class="flex h-screen flex-col bg-zinc-950">
+    <div class="flex h-12 items-center justify-between border-b border-zinc-800 px-4">
+      <span class="text-sm font-semibold text-zinc-200">{currentChannel.channelName}</span>
+      <button
+        onclick={() => showChat = !showChat}
+        class="rounded-lg px-3 py-1 text-xs transition
+               {showChat ? 'bg-indigo-600 text-white' : 'bg-zinc-700 text-zinc-300 hover:bg-zinc-600'}"
+      >
+        {showChat ? 'Hide Chat' : 'Show Chat'}
+      </button>
+    </div>
+    <div class="flex flex-1 overflow-hidden">
+      <div class="flex flex-1 flex-col">
+        <VoiceGrid />
+        <VoiceControls onLeave={() => history.back()} />
+      </div>
+      {#if showChat}
+        <div class="flex w-72 flex-col border-l border-zinc-800">
+          <ChatMessages currentMemberId={currentMember.memberId} />
+          <ChatInput channelName={currentChannel.channelName} />
+        </div>
+      {/if}
+    </div>
   </div>
 
 {:else}
